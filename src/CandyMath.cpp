3973b40ff42e908abe25dc8864c5c1f83cd6bf9f
Renaud GAUTIER <gautierenaud@gmail.com>
2014-02-26 11:53:57 +0100

    Create CandyMath in src

---
+++ src/CandyMath.cpp
@@ -0,0 +1,30 @@
+#include "CandyMath.h"
+#include <cmath>
+
+using namespace Candy;
+
+bool Vector::isNormalised() const {
+  return length()==1;
+}
+
+void Vector::normalize(){
+  Real norme = length();
+  x=x/norme;
+  y=y/norme;
+}
+
+Real Vector::length() const {
+  return sqrt(x*x+y*y);
+}
+
+Vector Vector::operator+(const Vector & v) const {
+  return {x+v.x, y+v.y};
+}
+
+Vector Vector::operator-(const Vector & v) const {
+  return {x-v.x, y-v.y};
+}
+
+Vector Vector::operator*(const Real & r) const {
+
+}

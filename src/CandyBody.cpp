f291ea62f14a4a11501606029176d490d14cbc50
Frank Bessou <fbessou@free.fr>
2014-02-27 15:02:43 +0100

    Modification de la classe Body.
    La forme est stockée dans un union.

--- src/CandyBody.cpp
+++ src/CandyBody.cpp
@@ -2,3 +2,19 @@
 
 using namespace Candy;
 
+Body::Body(const Rectangle & rect)
+{
+	mType = RECTANGLE;
+}
+
+Body::Body(const Circle & circle)
+{
+	mType = CIRCLE;
+}
+
+Body::Body(const ConvexHull & hull)
+{
+	mType = ConvexHull;
+}
+
+Body::

ddf55e8726ac44e51348511077b8e10320340e8e
Renaud GAUTIER <gautierenaud@gmail.com>
2014-02-27 21:05:54 +0100

    Write methods for CandyMath

--- include/CandyMath.h
+++ include/CandyMath.h
@@ -3,6 +3,8 @@
 
 #include "CandyCommon.h"
 
+#include <ostream>
+
 namespace Candy
 {
 	class Vector
@@ -10,7 +12,7 @@ namespace Candy
 		public:
 			Real x,y;
 		public:
-			Vector(const Real & ,const Real & , const Real & );
+			Vector(const Real & ,const Real &);
 			~Vector();
 
 			bool isNormalized() const;
@@ -27,13 +29,15 @@ namespace Candy
 			const Vector & operator-=(const Vector &);
 			const Vector & operator*=(const Real &);
 			const Vector & operator/=(const Real &);
-			Real angleBetween(Vec)
+			//Real angleBetween(Vec)
 			Real length() const; // return the magnitude of the vector
 			Real squaredLength() const; // return the squared magnitude of the vector (much faster)
 		public:
-			static const UNIT_X;
-			static const UNIT_Y;
+			static const Vector UNIT_X;
+			static const Vector UNIT_Y;
 	};
+
+	std::ostream& operator<<(std::ostream& stream, const Vector& v);
 
 	namespace Math
 	{
--- src/CandyMath.cpp
+++ src/CandyMath.cpp
@@ -1,9 +1,20 @@
 #include "CandyMath.h"
 #include <cmath>
+#include <ostream>
 
 using namespace Candy;
 
-bool Vector::isNormalised() const {
+const Vector Vector::UNIT_X(1.0, 0.0);
+const Vector Vector::UNIT_X(0.0, 1.0);
+
+Vector::Vector(const Real & a, const Real & b){
+  x = a;
+  y = b;
+}
+
+Vector::~Vector(){}
+
+bool Vector::isNormalized() const {
   return length()==1;
 }
 
@@ -17,6 +28,10 @@ Real Vector::length() const {
   return sqrt(x*x+y*y);
 }
 
+Real Vector::squaredLength() const {
+  return x*x+y*y;
+}
+
 Vector Vector::operator+(const Vector & v) const {
   return {x+v.x, y+v.y};
 }
@@ -27,4 +42,38 @@ Vector Vector::operator-(const Vector & v) const {
 
 Vector Vector::operator*(const Real & r) const {
   return {r*x, r*y};
+}
+
+Vector Vector::operator/(const Real & r) const {
+  return {x/r, y/r};
+}
+
+const Vector & Vector::operator=(const Vector & v){
+  x=v.x;
+  y=v.y;
+}
+
+const Vector & Vector::operator+=(const Vector & v){
+  x+=v.x;
+  y+=v.y;
+}
+
+const Vector & Vector::operator-=(const Vector & v){
+  x-=v.x;
+  y-=v.y;
+}
+
+const Vector & Vector::operator*=(const Real & v){
+  x*=v;
+  y*=v;
+}
+
+const Vector & Vector::operator/=(const Real & v){
+  x/=v;
+  y/=v;
+}
+
+std::ostream& operator<<(std::ostream& stream, const Vector& v){
+  stream<<"Vector("<<v.x<<","<<v.y<<")";
+  return stream;
 }

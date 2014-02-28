5bbe6ad9ad777571e3834ba40b4c3a119c8e1deb
Frank Bessou <fbessou@free.fr>
2014-02-28 23:14:42 +0100

    Suppression de lignes de diff...

--- src/CandyMath.cpp
+++ src/CandyMath.cpp
@@ -3,24 +3,14 @@
 #include <ostream>
 
 using namespace Candy;
-Vector::Vector(const Real & _x, const Real & _y): x(_x), y(_y){}
 
-<<<<<<< HEAD
-Vector::~Vector(){}
-bool Vector::isNormalised() const {
-=======
 const Vector Vector::UNIT_X(1.0, 0.0);
-const Vector Vector::UNIT_X(0.0, 1.0);
+const Vector Vector::UNIT_Y(0.0, 1.0);
 
-Vector::Vector(const Real & a, const Real & b){
-  x = a;
-  y = b;
-}
-
+Vector::Vector(const Real & _x, const Real & _y): x(_x), y(_y){}
 Vector::~Vector(){}
 
-bool Vector::isNormalized() const {
->>>>>>> ddf55e8726ac44e51348511077b8e10320340e8e
+bool Vector::isNormalised() const {
   return length()==1;
 }
 
@@ -50,8 +40,10 @@ Vector Vector::operator-(const Vector & v) const {
 Vector Vector::operator*(const Real & r) const {
   return {r*x, r*y};
 }
+Vector Vector::operator/(const Real & r) const {
+  return {x/r, y/r};
+}
 
-<<<<<<< HEAD
 const Vector & Vector::operator=(const Vector & vect)
 {
 	x=vect.x;
@@ -86,38 +78,9 @@ const Vector & Vector::operator/=(const Real & k)
 	x/=k;
 	y/=k;
 	return *this;
-=======
-Vector Vector::operator/(const Real & r) const {
-  return {x/r, y/r};
-}
-
-const Vector & Vector::operator=(const Vector & v){
-  x=v.x;
-  y=v.y;
-}
-
-const Vector & Vector::operator+=(const Vector & v){
-  x+=v.x;
-  y+=v.y;
-}
-
-const Vector & Vector::operator-=(const Vector & v){
-  x-=v.x;
-  y-=v.y;
-}
-
-const Vector & Vector::operator*=(const Real & v){
-  x*=v;
-  y*=v;
-}
-
-const Vector & Vector::operator/=(const Real & v){
-  x/=v;
-  y/=v;
 }
 
 std::ostream& operator<<(std::ostream& stream, const Vector& v){
   stream<<"Vector("<<v.x<<","<<v.y<<")";
   return stream;
->>>>>>> ddf55e8726ac44e51348511077b8e10320340e8e
 }

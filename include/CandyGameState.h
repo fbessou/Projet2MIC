7ae19b9b8faee1e8d423a55d11db4de83f3a0383
Frank Bessou <fbessou@free.fr>
2014-03-02 02:58:48 +0100

    Ajout des modifications necessaires pour lancer le jeu
    Ajout des nouveaux .o au makefile,
    Ajout d'un destructeur virtuel au GameState,
    Ajout d'une fonction de lancement,
    Lancement du "jeu" dans le main.

--- include/CandyGameState.h
+++ include/CandyGameState.h
@@ -1,15 +1,16 @@
 #ifndef _CANDY_GAMESTATE_H_
-#ifndef _CANDY_GAMESTATE_H_
+#define _CANDY_GAMESTATE_H_
 
 namespace Candy
 {
 	class GameState
 	{
 		public:
+			virtual ~GameState(); // Needed to permit polymorphic destruction
 			virtual void enter() = 0;
-			virtual void update() = 0;
+			virtual bool update() = 0;
 			virtual void leave() = 0;
 	};
-
+inline GameState::~GameState(){}
 };
 #endif 

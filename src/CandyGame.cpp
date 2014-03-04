2eb7ed4bc0cfbc3ed768b9174701e4295692b5f7
gautierenaud <gautierenaud@gmail.com>
2014-03-04 19:00:32 +0100

    Allow CandyMain and CandyMainMenu to open Window

--- src/CandyGame.cpp
+++ src/CandyGame.cpp
@@ -15,12 +15,24 @@ Game::~Game()
 void Game::start()
 {
 	mWindow = new sf::RenderWindow(sf::VideoMode(800, 600), "CandySaga");
+	mWindow->setFramerateLimit(60);
+
 	mCurrentState=new MainMenu(mWindow);
 	//some init
+
 	while(!hasExited)
 	{
+
+		sf::Event event;
+		while (mWindow->isOpen() && mWindow->pollEvent(event)){
+			if(event.type == sf::Event::Closed)
+			{
+				hasExited=true;
+			}
+		}
 		//update inputs
 		mCurrentState->update();
 	}
+	mWindow->close();
 }
 
--- src/CandyMainMenu.cpp
+++ src/CandyMainMenu.cpp
@@ -6,7 +6,7 @@ using namespace sf;
 
 MainMenu::MainMenu(RenderWindow * window):GameState(window)
 {
-	
+	mWindow->setTitle("Candy Saga 3 Le Retour des Caries  ~Menu~");	
 }
 
 MainMenu::~MainMenu()
@@ -15,7 +15,12 @@ MainMenu::~MainMenu()
 
 void MainMenu::enter(){}
 void MainMenu::leave(){}
+
+int frame=0;
+
 bool MainMenu::update(){
-	std::cout<<"Hello"<<std::endl;
+	mWindow->clear(Color(0,50,frame%256,255));
+	frame++;
+	mWindow->display();
 return true;
 }

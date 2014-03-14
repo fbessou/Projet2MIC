de30e61691e52718d2e3c5a5db85c91f6b4e4595
Renaud GAUTIER <gautierenaud@gmail.com>
2014-03-14 18:01:30 +0100

    Aff more menu options

--- src/CandyMainMenu.cpp
+++ src/CandyMainMenu.cpp
@@ -3,23 +3,18 @@
 
 using namespace Candy;
 using namespace sf;
-
-MainMenu::MainMenu(Game * game, RenderWindow * window):GameState(game,window),
-playTxt("Play the Game",game->getFont(),30)
+double a = 0.0;
+MainMenu::MainMenu(Game * game, RenderWindow * window):
+  GameState(game,window), playTxt("Play the Game",game->getFont(),30), paramTxt("Parameters",game->getFont(),30),quitTxt("Allez voir le dentiste",game->getFont(),30)
 {
 	playTxt.setPosition(0.5,0.5);
 	sf::FloatRect textRect = playTxt.getLocalBounds();
-	playTxt.setOrigin(textRect.left + textRect.width/2.0f,
-			textRect.top  + textRect.height/2.0f);
-	playTxt.setPosition(sf::Vector2f(mWindow->getSize().x/2.0f,mWindow->getSize().y/2.0f));
+	playTxt.setOrigin(textRect.left + textRect.width/2.0f,textRect.top  + textRect.height/2.0f);
+	//playTxt.setPosition(sf::Vector2f(mWindow->getSize().x/2.0f,mWindow->getSize().y/2.0f));
+	playTxt.setPosition(sf::Vector2f(0,mWindow->getSize().y/2.0f));
 	mWindow->setTitle("Candy Saga 3 Le Retour des Caries  ~Menu~");
-	//	playTxt.setColor(Color::White);
-	/*
-	   options.setString("Parameters");
-	   options.setColor(Color::Red);
-
-	   quit.setString("Aller voir le dentiste");
-	   quit.setColor(Color::Yellow); */
+	paramTxt.setPosition(sf::Vector2f(0,mWindow->getSize().y/5.0*3.0));
+	quitTxt.setPosition(sf::Vector2f(0,mWindow->getSize().y/5.0*4.0));
 }
 
 MainMenu::~MainMenu()
@@ -30,8 +25,15 @@ void MainMenu::enter(){}
 void MainMenu::leave(){}
 
 bool MainMenu::update(){
+  if(a<1)
+    {
+    playTxt.setPosition(sf::Vector2f(a*mWindow->getSize().x/2,mWindow->getSize().y/2.0f));
+    a+=0.01;
+    }
 	mWindow->clear();
 	mWindow->draw(playTxt);
+	mWindow->draw(paramTxt);
+	mWindow->draw(quitTxt);
 	mWindow->display();
 	return true;
 }

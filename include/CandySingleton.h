734f369b0f5df714e9d432b7948b612758763fd9
Frank Bessou <fbessou@free.fr>
2014-04-23 11:08:10 +0200

    Oubli de CandySingleton.h

---
+++ include/CandySingleton.h
@@ -0,0 +1,24 @@
+#ifndef _SINGLETON_H_
+#define _SINGLETON_H_
+
+namespace Candy{
+	template <typename T>
+	class Singleton
+	{
+		protected:
+			static T * mInstance;
+			~Singleton(){};
+			Singleton(const Singleton &);
+		public:
+			Singleton(){
+			mInstance=(T*)this;
+			};
+			static T & getInstance()
+			{
+				return mInstance;
+			}
+	};
+
+};
+template<typename T> T * Candy::Singleton<T>::mInstance= nullptr;
+#endif
